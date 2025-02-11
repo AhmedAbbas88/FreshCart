5f87c199608ceb9a1a6ea5d62ec6462b6352f3d6
Ahmed Abbas <154712858+AhmedAbbas88@users.noreply.github.com>
2025-02-11 23:47:33 +0200

    Update Layout

--- src/Component/Layout/Layout.jsx
+++ src/Component/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import style from './Layout.module.css'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
 import { Outlet } from 'react-router-dom'
-import { Offline, Online } from "react-detect-offline";
+// import { Offline, Online } from "react-detect-offline";
 
 export default function Layout() {
 
@@ -13,9 +13,9 @@ export default function Layout() {
   <Navbar/>
   <div className="container">
 
-
+{/* 
     <Online></Online>
-    <Offline> <div className="loading"><h2 className='fw-bold'>You're Offline</h2> </div></Offline>
+    <Offline> <div className="loading"><h2 className='fw-bold'>You're Offline</h2> </div></Offline> */}
     <Outlet></Outlet>
 
 
